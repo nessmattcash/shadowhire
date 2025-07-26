a8bec09f5c227f0158f2a85b6079fb6e38b740f6
nessmattcash <azizmehdii21@gmail.com>
2025-07-26 20:40:41 +0100

    front fixing

---
+++ frontend/shadowhire/src/app/components/backoffice/candidate-management/candidate-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-candidate-management',
+  imports: [],
+  templateUrl: './candidate-management.component.html',
+  styleUrl: './candidate-management.component.scss'
+})
+export class CandidateManagementComponent {
+
+}
---
+++ frontend/shadowhire/src/app/components/backoffice/ml-feedback-panel/ml-feedback-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ml-feedback-panel',
+  imports: [],
+  templateUrl: './ml-feedback-panel.component.html',
+  styleUrl: './ml-feedback-panel.component.scss'
+})
+export class MlFeedbackPanelComponent {
+
+}
