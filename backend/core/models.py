93cb37aedda305aa5c392acfc1af717b562ac988
nessmattcash <azizmehdii21@gmail.com>
2025-09-08 19:16:55 +0100

    testing the parsing model + update user backend logic + add jobs for the next model jobmatching

--- backend/core/models.py
+++ backend/core/models.py
@@ -5,8 +5,9 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    is_recruiter = models.BooleanField(default=False)  # True for recruiters/admins (back office), False for normal users (front office)
-
+    is_recruiter = models.BooleanField(default=False)  
+    is_admin = models.BooleanField(default=False)
+    company_name = models.CharField(max_length=100, blank=True, null=True) 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.email})"
 
