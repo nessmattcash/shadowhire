4ccbcdf58543377860d2131b547b013d511a3c3f
nessmattcash <azizmehdii21@gmail.com>
2025-09-10 03:58:53 +0100

    start working on jobs and jobs-details interfaces + improving in prasing model

---
+++ frontend/shadowhire/src/app/services/jobs.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+interface Job {
+  id: number;
+  title: string;
+  description: string;
+  company: string;
+  location: string;
+  created_by: string;
+  created_at: string;
+  skills_required: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JobsService {
+  private baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getJobs(): Observable<Job[]> {
+    return this.http.get<Job[]>(`${this.baseUrl}/jobs/`);
+  }
+}
